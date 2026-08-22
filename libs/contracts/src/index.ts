97cde42fc702136291e92669efc86e37ab89e1d4
SaranD <sdoniparthi@miyohealth.com>
2026-08-22 14:06:47 -0500

    feat: add scheduling contracts

--- libs/contracts/src/index.ts
+++ libs/contracts/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/contracts';
+export * from './lib/appointments/appointment-contracts';
 export * from './lib/grpc/agent-runtime';
 export * from './lib/privacy/sensitive-fields';
